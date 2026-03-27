355dec8aaa65535593b23b36fe5456aff360339e
sgcodes7471 <official06srinjoy@gmail.com>
2026-03-27 23:17:46 +0530

    fix : CORS configuration added via env

--- backend-fastapi/main.py
+++ backend-fastapi/main.py
@@ -38,7 +38,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware , 
-    allow_origins = ["*"] , 
+    allow_origins = [os.getenv("ALLOWED_ORIGINS")] , 
     allow_credentials = True , 
     allow_methods = ["*"],
     allow_headers = ["*"]
